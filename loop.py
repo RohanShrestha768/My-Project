db31afb3f39ba826181243c9e271a41b29b43c7b
Rajan shrestha <68985809+ray5123@users.noreply.github.com>
2024-04-06 18:49:21 +0000

    nested loop added

---
+++ loop.py
@@ -0,0 +1,12 @@
+row=int(input("How many rows? :"))
+col=int(input("How many columns? :"))
+col=row+row-5
+mid=col//2
+for i in range(row):
+    for j in range(col):
+        if i==2 or i==row-3 or i+j==mid or j-i== mid or i-j==2 or i+j==col+1:
+            print("@", end="")
+        else:
+            print(" ",end="")
+    print()
+        
